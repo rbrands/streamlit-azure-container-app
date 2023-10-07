822bd7ee565e52cfee5954bcbc9aa2060eda0843
Robert Brands <mail@robert-brands.com>
2023-10-07 07:02:55 +0000

    Codespace configured

---
+++ src/Frontend/Start.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+"""
+# Azure Container App Demo with Streamlit
+
+This is a demo application to show:
+- How to host a Streamlit-App with a Azure Function Backend in Azure Container App
+- Python/Streamlit development mit GitHub Codespaces
+
+See GitHub repo for details: https://github.com/rbrands/streamlit-azure-container-app
+"""
